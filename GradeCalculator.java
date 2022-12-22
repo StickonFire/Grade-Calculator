8c7309931fb3679461b127df79bb62383c6f3e98
StickonFire <70039286+StickonFire@users.noreply.github.com>
2022-12-22 01:22:37 -0800

    Added Assignment class
    Established the Assignment class, along with completed methods getScore, toSTring and a constructor.
    
    Also added a new static integer in GradeCalculator percentage, simply meant as a means to shift a number to a percentage.

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -1,6 +1,7 @@
 public class GradeCalculator {
 
     static String className = "No name";
+    static int percentage = 100;
 
     /**
      * Basic main function of GradeCalculator, and deals with getting the algorithm
@@ -15,5 +16,44 @@ public class GradeCalculator {
                 className = args[i] + " " + className;
             }
         }
+
+
+    }
+}
+
+/**
+ * A representation of an assignment, with a name and a score.
+ * Operates as the most basic Node and effectively is a leaf for a tree.
+ */
+class Assignment {
+
+    String name;
+    int achievedPoints;
+    int maxPoints;
+
+    /**
+     * Basic constructor for the Assignment class. 
+     * Given all three intended values, the constructor simply sets the given values
+     * to the corresponding value
+     * @param name - intended value for name
+     * @param achievedPoints - intended value for achievedPoints
+     * @param maxPoints - intended value for maxPoints
+     */
+    Assignment(String name, int achievedPoints, int maxPoints){
+        this.name = name;
+        this.achievedPoints = achievedPoints;
+        this.maxPoints = maxPoints;
+    }
+    
+    /**
+     * Returns the score percentage of the calling assignment.
+     * @return score percentage of the assignment.
+     */
+    int getScore(){
+        return achievedPoints*GradeCalculator.percentage/maxPoints;
+    }
+
+    public String toString(){
+        return name;
     }
 }
