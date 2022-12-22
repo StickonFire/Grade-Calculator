7ab83dc281b7052cba9a1600ed51fac1661fe801
StickonFire <70039286+StickonFire@users.noreply.github.com>
2022-12-22 15:30:58 -0800

    Added testAssignment tests
    Added to GradecalculatorTester:
     - new Assignments objects zeroed, normal, MaxPoints0, and NegPoints to use within tests.
     - Added new tests for Assignment, when the given points are normal, maxPoints, all zeroed, and negative achievedPoints.

--- GradeCalculatorTester.java
+++ GradeCalculatorTester.java
@@ -1,3 +1,26 @@
 public class GradeCalculatorTester {
-    Assignment num = new Assignment(null, 0, 0);
+    Assignment zeroed = new Assignment(null, 0, 0);
+    Assignment Normal = new Assignment("Prim",10,20);
+    Assignment MaxPoints0 = new Assignment("",55,0);
+    Assignment NegPoints = new Assignment("NegPoints", -5, 5);
+
+    /**
+     * A basic test for the Assignment class. 
+     * Tests the following items:
+     *  getScore()
+     *   - check if maxPoints is 0
+     *   - check a normal
+     *   - check if everything is 0
+     *   - check if points are negative
+     *  getString()
+     *   - check if name is null, should cause "No name"
+     *   - empty name
+     */
+    @Test
+    void testAssignment(){
+        assertEquals("This test checks a normal score (achievedPoints is 0)", Normal.getScore(), 50);
+        assertEquals("This test checks when maxPoints is 0", MaxPoints0.getScore(), 5500);
+        assertEquals("This test checks when everything is 0", zeroed.getScore(), 0);
+        assertEquals("This test checks when achievedPoints is negative",NegPoints.getScore(),-100);
+    }
 }
