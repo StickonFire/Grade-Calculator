bea80b99bd3f3b937f29a4c51b16b551cf776e90
StickonFire <70039286+StickonFire@users.noreply.github.com>
2022-12-23 21:28:30 -0800

    Test getScore and toString in testAssignment
    Changed testAssignment to test an assignment's getScore and toString methods. Checks the following cases:
    getScore:
     - normal(either points is nonzero and nonnegative)
     - maxPoints is 0
     - everything is 0
     - achievedPoints is negative
    toString:
     - null name
     - regular name (non empty string)
     - empty string

--- GradeCalculatorTester.java
+++ GradeCalculatorTester.java
@@ -1,11 +1,16 @@
-import static org.junit.Assert.*;
 import org.junit.*;
+import static org.junit.Assert.*;
+
 
 public class GradeCalculatorTester {
     Assignment zeroed = new Assignment(null, 0, 0);
+    int[] score1 = {0,0};
     Assignment Normal = new Assignment("Prim",10,20);
+    int[] score2 = {10,20};
     Assignment MaxPoints0 = new Assignment("",55,0);
+    int[] score3 = {55,0};
     Assignment NegPoints = new Assignment("NegPoints", -5, 5);
+    int[] score4 = {-5,5};
 
     /**
      * A basic test for the Assignment class. 
@@ -20,10 +25,14 @@ public class GradeCalculatorTester {
      *   - empty name
      */
     @Test
-    void testAssignment(){
-        assertEquals("This test checks a normal score (achievedPoints is 0)", Normal.getScore(), 50);
-        assertEquals("This test checks when maxPoints is 0", MaxPoints0.getScore(), 5500);
-        assertEquals("This test checks when everything is 0", zeroed.getScore(), 0);
-        assertEquals("This test checks when achievedPoints is negative",NegPoints.getScore(),-100);
+    public void testAssignment(){
+        assertArrayEquals("This test checks a normal score (achievedPoints is 0)", Normal.getScore(), score2);
+        assertArrayEquals("This test checks when maxPoints is 0", MaxPoints0.getScore(), score3);
+        assertArrayEquals("This test checks when everything is 0", zeroed.getScore(), score1);
+        assertArrayEquals("This test checks when achievedPoints is negative",NegPoints.getScore(),score4);
+
+        assertEquals("This string checks if the constructor gives a null name","No name",zeroed.toString());
+        assertEquals("This string checks if constructor gives a valid name","NegPoints",NegPoints.toString());
+        assertEquals("This string checks if constructor gives an empty name","",MaxPoints0.toString());
     }
 }
