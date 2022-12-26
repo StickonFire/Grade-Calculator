36a5ca4eab57b45ab260fa0bff27190ca3814674
StickonFire <70039286+StickonFire@users.noreply.github.com>
2022-12-26 03:18:11 -0800

    Preparations for GradeCalculator.getScore tests
    Added a few arrays to use for testing the getScores method of GradeCalculator.
    
    Also added a description of tests for GradeCalculator.getScore().

--- GradeCalculatorTester.java
+++ GradeCalculatorTester.java
@@ -1,8 +1,20 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 
 public class GradeCalculatorTester {
+    GradeCalculator test = new GradeCalculator();
+
+    //Note: if trying again, just add the below Assignments to the end of the assignment list to be created in tests.
+    Assignment[] oneNormList = {new Assignment("A",10,20)};
+    Assignment[] emptyList = new Assignment[0];
+    Assignment[] zeroedList = {new Assignment(null,0,0)};
+    Assignment[] MaxPoints0List = {new Assignment("B",15,0)};
+    Assignment[] NegPointsList = {new Assignment("C",-5,4)};
+
+
     Assignment zeroed = new Assignment(null, 0, 0);
     int[] score1 = {0,0};
     Assignment Normal = new Assignment("Prim",10,20);
@@ -34,5 +46,21 @@ public class GradeCalculatorTester {
         assertEquals("This string checks if the constructor gives a null name","No name",zeroed.toString());
         assertEquals("This string checks if constructor gives a valid name","NegPoints",NegPoints.toString());
         assertEquals("This string checks if constructor gives an empty name","",MaxPoints0.toString());
+    }
+
+    /**
+     * Test for the GradeCalculator.getScore() method)
+     * It checks the following cases:
+     *  - No assignments
+     *  - MaxPoints adds up to 0.
+     *  - Just Normal scores
+     *  - Just all 0 or null scores
+     *  - all assignments have 0 maxPoints
+     *  - MaxPoints are negative
+     *  - MaxPoints is negative and achievedPoints is positive.
+     */
+    @Test
+    public void testMainGetScore(){
+        
     }
 }
